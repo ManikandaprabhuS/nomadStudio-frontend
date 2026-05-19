a4cabce8c689003d03e5081e42ad33810e2a9d45
ManikandaprabhuS <sivakumarmanicrazy@gmail.com>
2026-05-19 14:23:38 +0530

    frontend updated

--- invoice-frontend/src/app/pages/login/login.ts
+++ invoice-frontend/src/app/pages/login/login.ts
@@ -29,7 +29,7 @@ export class Login {
     }
     console.log("Logged IN Service Called")
     //console.log('Logging in with', this.userName, this.password);
-    this.http.post<any>('https://nomadstudio-backend-vefx.onrender.com/auth/login', {
+    this.http.post<any>('https://nomadstudio-backend.onrender.com/auth/login', {
       userName: this.userName,
       password: this.password
 
