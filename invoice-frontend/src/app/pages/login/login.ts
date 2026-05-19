205c827d02c1ace01c9132b6cd3e766d36e55451
SivakumarMani <43170048+ManikandaprabhuS@users.noreply.github.com>
2026-05-19 15:35:53 +0530

    Update login.ts

--- invoice-frontend/src/app/pages/login/login.ts
+++ invoice-frontend/src/app/pages/login/login.ts
@@ -29,7 +29,7 @@ export class Login {
     }
     console.log("Logged IN Service Called")
     //console.log('Logging in with', this.userName, this.password);
-    this.http.post<any>('https://nomadstudio-backend.onrender.com/auth/login', {
+    this.http.post<any>('https://nomadstudio-backend-ct47.onrender.com/auth/login', {
       userName: this.userName,
       password: this.password
 
@@ -50,7 +50,7 @@ export class Login {
       alert('Please enter your email address');
       return;
     }
-    this.http.post<any>('https://nomadstudio-backend.onrender.com/auth/forgot-password', {
+    this.http.post<any>('https://nomadstudio-backend-ct47.onrender.com/auth/forgot-password', {
       emailId: this.emailId
     }).subscribe({
       next: (res) => {
@@ -68,7 +68,7 @@ export class Login {
       alert('Please fill in all fields');
       return;
     }
-    this.http.post<any>('https://nomadstudio-backend.onrender.com/auth/reset-password', {
+    this.http.post<any>('https://nomadstudio-backend-ct47.onrender.com/auth/reset-password', {
       emailId: this.emailId,
       otp: this.otp,
       newPassword: this.newPassword
